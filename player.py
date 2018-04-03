4bddceaf7062893f8f6680386fe0dfcc99313812
101hof010 <mheckel@hof-university.de>
2018-04-03 10:44:58 +0200

    Foo

--- player.py
+++ player.py
@@ -6,7 +6,11 @@ class Player:
         current_buy_in = game_state['current_buy_in']
         players = game_state['players']
         minimum_raise = game_state['minimum_raise']
-        return current_buy_in - players[0][bet] + minimum_raise + 42
+        stack = game_state['players'][0]['stack']
+        if stack > current_buy_in - players[0][bet] + minimum_raise + 42:
+            return current_buy_in - players[0][bet] + minimum_raise + 42
+        else:
+            return 0
 
     def showdown(self, game_state):
         pass
