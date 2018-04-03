bfb815d2795ad1858833d6a5237984f3bacb8065
101hof010 <mheckel@hof-university.de>
2018-04-03 10:47:08 +0200

    Foo

--- player.py
+++ player.py
@@ -1,16 +1,20 @@
+import sys
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        current_buy_in = game_state['current_buy_in']
-        players = game_state['players']
-        minimum_raise = game_state['minimum_raise']
-        stack = game_state['players'][0]['stack']
-        if stack > current_buy_in - players[0][bet] + minimum_raise + 42:
-            return current_buy_in - players[0][bet] + minimum_raise + 42
-        else:
-            return 0
+        try:
+            current_buy_in = game_state['current_buy_in']
+            players = game_state['players']
+            minimum_raise = game_state['minimum_raise']
+            stack = game_state['players'][0]['stack']
+            if stack > current_buy_in - players[0][bet] + minimum_raise + 42:
+                return current_buy_in - players[0][bet] + minimum_raise + 42
+            else:
+                return 0
+        except Exception as e:
+            sys.stderr.write("There was a Problem: " + str(e))
 
     def showdown(self, game_state):
         pass
