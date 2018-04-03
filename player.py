09ed103ae55b081b7527d80ee54041e02ea7acfe
101hof010 <mheckel@hof-university.de>
2018-04-03 11:50:34 +0200

    Foo

--- player.py
+++ player.py
@@ -19,7 +19,7 @@ class Player:
                 cards.append(card)
             for card in game_state['community_cards']:
                 cards.append(card)
-            max_amount = check_cards(cards) * 120
+            max_amount = self.check_cards(cards) * 120
             if max_amount > current_buy_in - players[index]['bet'] + minimum_raise + 42:
                 if stack > current_buy_in - players[index]['bet'] + minimum_raise + 42:
                     sys.stderr.write("\n\n### We can set " + str(current_buy_in - players[0]['bet'] + minimum_raise + 42) + "\n\n")
