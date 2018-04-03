4c6083dfc27f6354bcefac67d224d0cf9c86defe
101hof010 <mheckel@hof-university.de>
2018-04-03 10:41:30 +0200

    Foo

--- player.py
+++ player.py
@@ -3,7 +3,10 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 42
+        current_buy_in = game_state['current_buy_in']
+        players = game_state['players']
+        minimum_raise = game_state['minimum_raise']
+        return current_buy_in - players[0][bet] + minimum_raise + 42
 
     def showdown(self, game_state):
         pass
