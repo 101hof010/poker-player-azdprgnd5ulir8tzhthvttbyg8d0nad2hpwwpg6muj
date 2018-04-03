01a14f377f4b4def473dc0096e58d1d34d9ccb91
101hof010 <mheckel@hof-university.de>
2018-04-03 11:10:18 +0200

    Foo

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        sys.stderr.write("### Doing a turn.")
+        sys.stderr.write("\n\n### Doing a turn.\n\n")
         try:
             current_buy_in = game_state['current_buy_in']
             players = game_state['players']
@@ -14,14 +14,14 @@ class Player:
                 if players[i]['name'] == 'azDpRGnd5ULir8TzHtHvttByG8D0nAd2hPWwpg6MUJ':
                     index = i
             if stack > current_buy_in - players[index]['bet'] + minimum_raise + 42:
-                sys.stderr.write("### We can set " + str(current_buy_in - players[0]['bet'] + minimum_raise + 42))
+                sys.stderr.write("\n\n### We can set " + str(current_buy_in - players[0]['bet'] + minimum_raise + 42) + "\n\n")
                 return current_buy_in - players[0]['bet'] + minimum_raise + 42
             else:
-                sys.stderr.write("### We can't set")
+                sys.stderr.write("\n\n### We can't set\n\n")
                 return 0
         except Exception as e:
-            sys.stderr.write("### There was a Problem: " + str(e))
-            sys.stderr.write("Data: " + str(game_state))
+            sys.stderr.write("\n\n### There was a Problem: " + str(e) + "\n\n")
+            sys.stderr.write("\n\nData: " + str(game_state) + "\n\n")
         return 123
 
     def showdown(self, game_state):
