81714f79dca256c9026da9f4e4799317735278fb
101hof010 <mheckel@hof-university.de>
2018-04-03 16:44:06 +0200

    Foo

--- player.py
+++ player.py
@@ -53,7 +53,7 @@ class Player:
                 minimum_raise = 0
             max_amount = score * stack/100
             sys.stderr.write("\n\n### Going to a max of " + str(max_amount) + "\n\n")
-            if max_amount >= current_buy_in - players[index]['bet'] + minimum_raise:
+            if current_buy_in - players[index]['bet'] + minimum_raise <= max_amount:
                 sys.stderr.write("\n\n### We want to do it and have to set: " + str(current_buy_in - players[index]['bet'] + minimum_raise) + "\n\n")
                 if stack >= current_buy_in - players[index]['bet'] + minimum_raise:
                     sys.stderr.write("\n\n### We will set " + str(current_buy_in - players[index]['bet'] + minimum_raise) + "\n\n")
